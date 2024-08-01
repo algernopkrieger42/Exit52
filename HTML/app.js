e69f849487287501bc8a7912e70fe0bc78ea27a2
algernopkrieger42 <166349592+algernopkrieger42@users.noreply.github.com>
2024-07-31 22:30:40 -0700

    json working?
    ?

--- HTML/app.js
+++ HTML/app.js
@@ -1,13 +1,42 @@
+import fs from "fs";
 function displayPrediction() {
     document.getElementById("button").style.display = 'none';
     document.getElementById("float-container").style.display = 'block';
 }
 
-function readJSON() {
-    fetch('predictions.json')
-    .then(response => response.json())
-    var mydata = JSON.parse(response);
-    var date = mydata[0].Todays_Date;
-    var statment = date.concat(' ',mydata[0].Todays_Prediction);
+function todaysPrediction() { 
+    fs.readFile("./predictions.json", 'utf8', (error, data) => {
+        if (error){
+            console.log(error);
+            return;
+        }
+        var date = JSON.parse(data).Todays_Date;
+        var prediction = JSON.parse(data).Todays_Prediction;
+        var inches = "in";
+        var output = date +  " " + prediction + inches;
+        console.log(output);
+        return output;
+    })
 }
+
+function tomorrowsPrediction() { 
+    fs.readFile("./predictions.json", 'utf8', (error, data) => {
+        if (error){
+            console.log(error);
+            return;
+        }
+        var date = JSON.parse(data).Tomorrows_Date;
+        var prediction = JSON.parse(data).Tomorrows_Prediction;
+        var inches = "in";
+        var output = date +  " " + prediction + inches;
+        console.log(output);
+        return output;
+    })
+}
+
+function displayPredictions(){
+    todaysPrediction();
+    tomorrowsPrediction();
+}
+
 
--- HTML/testing.js
+++
@@ -1,6 +0,0 @@
-function fetchJSONData() {
-    fetch('./predictions.json')
-    .then((response) => JSON.parse())
-    .then((json) => console.log(json[0]));
-}
-fetchJSONData()
