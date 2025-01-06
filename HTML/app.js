71b9fc07ceb74d98c1e42445a28a3b8bb568e094
algernopkrieger42 <166349592+algernopkrieger42@users.noreply.github.com>
2025-01-06 13:50:03 -0700

    combining
    original cors header with auth

--- HTML/app.js
+++ HTML/app.js
@@ -45,7 +45,11 @@ function tomorrowsPrediction(json) {
 
 async function fetchAPI() {
     try {
-        const response = await fetch('https://52crewdata.loca.lt/predictions.json?auth=${66.219.228.229}');
+        const response = await fetch('https://52crewdata.loca.lt/predictions.json?auth=${66.219.228.229}', {
+            headers: {
+                'bypass-tunnel-reminder': 'bypass', // You can use any string value here
+            }
+        });
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
